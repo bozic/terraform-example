3660c4881d4d3189b4a8079440cada58c25736a7
bozic <bozic@users.noreply.github.com>
2026-09-02 11:01:38 +0000

    Refactor subnet resource for improved readability by aligning property assignments.

--- azure/terraform-module-virtual-network/azure-virtual-network/main.tf
+++ azure/terraform-module-virtual-network/azure-virtual-network/main.tf
@@ -31,15 +31,15 @@ locals {
 resource "azurerm_subnet" "this" {
   for_each = var.subnets
 
-  name                                      = each.key
-  resource_group_name                       = var.resource_group_name
-  virtual_network_name                      = local.vnet_name
-  address_prefixes                          = each.value.address_prefixes
-  default_outbound_access_enabled           = each.value.default_outbound_access_enabled
-  private_endpoint_network_policies         = each.value.private_endpoint_network_policies
+  name                                          = each.key
+  resource_group_name                           = var.resource_group_name
+  virtual_network_name                          = local.vnet_name
+  address_prefixes                              = each.value.address_prefixes
+  default_outbound_access_enabled               = each.value.default_outbound_access_enabled
+  private_endpoint_network_policies             = each.value.private_endpoint_network_policies
   private_link_service_network_policies_enabled = each.value.private_link_service_network_policies_enabled
-  service_endpoints                         = each.value.service_endpoints
-  service_endpoint_policy_ids               = each.value.service_endpoint_policy_ids
+  service_endpoints                             = each.value.service_endpoints
+  service_endpoint_policy_ids                   = each.value.service_endpoint_policy_ids
 }
 
 resource "azurerm_subnet_network_security_group_association" "this" {
